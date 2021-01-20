15b84ae76bc2f9a33e3cb427575d942d05372eb7
debu3223 <debabratamukherjee@trakinvest.com>
2021-01-20 14:31:58 +0530

    ChannelUser Mapper Added

---
+++ channelUserMapper.ts
@@ -0,0 +1,60 @@
+const channelUserentityJson = {
+  id : "id?",
+  userId : "userId?",
+  
+  channelId : "channelId?",
+  
+  isActive: "isActive?",
+  
+  user: "user?",
+  
+  channel : "channel?",
+  CommunityUrl:"CommunityUrl?"
+};
+
+const channelUserdtoJson = {
+  id : "id?",
+  userId : "userId?",
+  
+  channelId : "channelId?",
+  
+  isActive: "isActive?",
+  
+  user: "user?",
+  
+  channel : "channel?",
+  CommunityUrl:"CommunityUrl?"
+};
+
+const channelUserentityToDtoJson = {
+  id : "id?",
+  userId : "userId?",
+  
+  channelId : "channelId?",
+  
+  isActive: "isActive?",
+  
+  user: "user?",
+  
+  channel : "channel?",
+  CommunityUrl:"CommunityUrl?"
+};
+
+const channelUserdtoToEntityJson= {
+  id : "id?",
+  userId : "userId?",
+  
+  channelId : "channelId?",
+  
+  isActive: "isActive?",
+  
+  user: "user?",
+  
+  channel : "channel?",
+  CommunityUrl: "CommunityUrl?"
+};
+
+module.exports.channelUserentityJson = channelUserentityJson;
+module.exports.channelUserdtoJson = channelUserdtoJson;
+module.exports.channelUserentityToDtoJson = channelUserentityToDtoJson;
+module.exports.channelUserdtoToEntityJson = channelUserdtoToEntityJson;
