f6f2c0254d88549a6605b48ed846807a4b47b78c
AAYUSH SOMANI <aayush@trakinvest.com>
2021-03-02 19:14:11 +0530

    added new mapping

---
+++ globalSearchMapper.ts
@@ -0,0 +1,37 @@
+const globalSearchentityJson = {
+  Id: "Id?",
+  fieldType: "fieldType?",
+  fieldValue: "fieldValue?",
+  fieldId: "fieldId?",
+  totalRecords: "totalRecords?",
+  additionalDetails: "additionalDetails?",
+};
+
+const globalSearchdtoJson = {
+  Id: "Id?",
+  fieldType: "fieldType?",
+  fieldValue: "fieldValue?",
+  fieldId: "fieldId?",
+  additionalDetails: "additionalDetails?",
+};
+
+const globalSearchentityToDtoJson = {
+  Id: "Id?",
+  field_type: "fieldType?",
+  field_value: "fieldValue?",
+  field_id: "fieldId?",
+  additional_details: "additionalDetails?",
+};
+
+const globalSearchdtoToEntityJson = {
+  Id: "Id?",
+  fieldType: "field_type?",
+  fieldValue: "field_value?",
+  fieldId: "field_id?",
+  additionalDetails: "additional_details?",
+};
+
+module.exports.globalSearchentityJson = globalSearchentityJson;
+module.exports.globalSearchdtoJson = globalSearchdtoJson;
+module.exports.globalSearchentityToDtoJson = globalSearchentityToDtoJson;
+module.exports.globalSearchdtoToEntityJson = globalSearchdtoToEntityJson;
