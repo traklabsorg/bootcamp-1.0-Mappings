f6442c4c181ca0f7a1704f008702f54896e46a37
AAYUSH SOMANI <aayush@trakinvest.com>
2021-03-24 18:26:41 +0530

    added userId

--- subscriptionOrderMapper.ts
+++ subscriptionOrderMapper.ts
@@ -1,70 +1,74 @@
 
 const subscriptionOrderentityJson = {
-  Id:"Id?",
+  Id: "Id?",
   orderStatus: "orderStatus?",
   orderDateTime: "orderDateTime?",
   orderDetails: "orderDetails?",
   subscriptionId: "subscriptionId?",
   subscription: "subscription?",
   payment: "payment?",
+  userId: "userId?",
   ModifiedBy: "ModifiedBy?",
   CreatedBy: "CreatedBy?",
   CreationDate: "CreationDate?",
   ModifiedDate: "ModifiedDate?"
-   //SocketId: "SocketId?",
-      
-  };
-  
-const subscriptionOrderdtoJson = {
-  Id:"Id?",
-  orderStatus: "orderStatus?",
-  orderDateTime: "orderDateTime?",
-  orderDetails: "orderDetails?",
-  subscriptionId: "subscriptionId?",
-  subscription: "subscription?",
-  payment: "payment?",
-  ModifiedBy: "ModifiedBy?",
-  CreatedBy: "CreatedBy?",
-  CreationDate: "CreationDate?",
-  ModifiedDate: "ModifiedDate?"
-   //SocketId: "SocketId?",
-      
-  };
-  
-const subscriptionOrderentityToDtoJson = {
-  Id:"Id?",
-  orderStatus: "orderStatus?",
-  orderDateTime: "orderDateTime?",
-  orderDetails: "orderDetails?",
-  subscriptionId: "subscriptionId?",
-  subscription: "subscription?",
-  payment: "payment?",
-  ModifiedBy: "ModifiedBy?",
-  CreatedBy: "CreatedBy?",
-  CreationDate: "CreationDate?",
-  ModifiedDate: "ModifiedDate?"
-   //SocketId: "SocketId?",
-      
-  };
-  
-const subscriptionOrderdtoToEntityJson = {
-  Id:"Id?",
-  orderStatus: "orderStatus?",
-  orderDateTime: "orderDateTime?",
-  orderDetails: "orderDetails?",
-  subscriptionId: "subscriptionId?",
-  subscription: "subscription?",
-  payment: "payment?",
-  ModifiedBy: "ModifiedBy?",
-  CreatedBy: "CreatedBy?",
-  CreationDate: "CreationDate?",
-  ModifiedDate: "ModifiedDate?"
-   //SocketId: "SocketId?",
-      
+  //SocketId: "SocketId?",
 
-  };
-  
-  module.exports.subscriptionOrderentityJson = subscriptionOrderentityJson;
-  module.exports.subscriptionOrderdtoJson = subscriptionOrderdtoJson;
-  module.exports.subscriptionOrderentityToDtoJson = subscriptionOrderentityToDtoJson;
-  module.exports.subscriptionOrderdtoToEntityJson = subscriptionOrderdtoToEntityJson;
+};
+
+const subscriptionOrderdtoJson = {
+  Id: "Id?",
+  orderStatus: "orderStatus?",
+  orderDateTime: "orderDateTime?",
+  orderDetails: "orderDetails?",
+  subscriptionId: "subscriptionId?",
+  subscription: "subscription?",
+  payment: "payment?",
+  userId: "userId?",
+  ModifiedBy: "ModifiedBy?",
+  CreatedBy: "CreatedBy?",
+  CreationDate: "CreationDate?",
+  ModifiedDate: "ModifiedDate?"
+  //SocketId: "SocketId?",
+
+};
+
+const subscriptionOrderentityToDtoJson = {
+  Id: "Id?",
+  orderStatus: "orderStatus?",
+  orderDateTime: "orderDateTime?",
+  orderDetails: "orderDetails?",
+  subscriptionId: "subscriptionId?",
+  subscription: "subscription?",
+  payment: "payment?",
+  userId: "userId?",
+  ModifiedBy: "ModifiedBy?",
+  CreatedBy: "CreatedBy?",
+  CreationDate: "CreationDate?",
+  ModifiedDate: "ModifiedDate?"
+  //SocketId: "SocketId?",
+
+};
+
+const subscriptionOrderdtoToEntityJson = {
+  Id: "Id?",
+  orderStatus: "orderStatus?",
+  orderDateTime: "orderDateTime?",
+  orderDetails: "orderDetails?",
+  subscriptionId: "subscriptionId?",
+  subscription: "subscription?",
+  payment: "payment?",
+  userId: "userId?",
+  ModifiedBy: "ModifiedBy?",
+  CreatedBy: "CreatedBy?",
+  CreationDate: "CreationDate?",
+  ModifiedDate: "ModifiedDate?"
+  //SocketId: "SocketId?",
+
+
+};
+
+module.exports.subscriptionOrderentityJson = subscriptionOrderentityJson;
+module.exports.subscriptionOrderdtoJson = subscriptionOrderdtoJson;
+module.exports.subscriptionOrderentityToDtoJson = subscriptionOrderentityToDtoJson;
+module.exports.subscriptionOrderdtoToEntityJson = subscriptionOrderdtoToEntityJson;
