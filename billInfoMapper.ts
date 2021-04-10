e8e53a19855440efa9c9b668d901a526434c2731
debu3223 <debabratamukherjee@trakinvest.com>
2021-04-10 10:52:51 +0530

    added billInfo mapper

---
+++ billInfoMapper.ts
@@ -0,0 +1,14 @@
+const bilInfoMapper = {
+    bill_id :"billId?",
+    bill_date : "billDate?",
+    title : "title?",
+    description : "description?",
+    total_users : "totalUsers?",
+    per_user_fee : "perUserFee?",
+    currency : "currency?",
+    tax : "tax?",
+    total_charges : "totalCharges?",
+    totalrecords : "totalrecords?"
+  };
+
+  module.exports.billInfoMapper = bilInfoMapper;
