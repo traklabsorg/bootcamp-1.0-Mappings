1edc822f86cc1ef84440d5535619f67ad89aaa34
debu3223 <debabratamukherjee@trakinvest.com>
2021-04-15 11:13:30 +0530

    added receiptInfo Mapper

---
+++ receiptsInfoMapper.ts
@@ -0,0 +1,12 @@
+const receiptsInfoMapper = {
+    group_name : "groupName?", 
+	user_name : "userName?",  
+	bill_amt : "billAmt?", 
+	cut_off : "cuttOff?",
+	channel_name : "channelName?", 
+	total_pay : "totalPay?", 
+	totalrecords : "totalRecords?" 
+  }
+
+
+  module.exports.receiptsInfoMapper = receiptsInfoMapper;
