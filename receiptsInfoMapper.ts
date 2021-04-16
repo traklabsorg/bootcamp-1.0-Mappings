c2084a9ec70c46fa51ea62021ae50615ad7a144f
debu3223 <debabratamukherjee@trakinvest.com>
2021-04-16 19:52:53 +0530

    changed billinfo mapper

--- receiptsInfoMapper.ts
+++ receiptsInfoMapper.ts
@@ -5,7 +5,21 @@ const receiptsInfoMapper = {
 	cut_off : "cuttOff?",
 	channel_name : "channelName?", 
 	total_pay : "totalPay?", 
-	totalrecords : "totalRecords?" 
+	totalrecords : "totalRecords?",
+	firstname : "firstname?", 
+	lastname : "lastname?", 
+	time_stamp : "timestamp?", 
+	transaction_id : "transactionId?", 
+	stripe_transaction_id : "stripeTransactionId?", 
+	email : "email?", 
+	charging_type : "chargingType?", 
+	channel_fee : "channelFee?", 
+	amount_collected : "amountCollected?", 
+	net_payout : "netPayout?",   
+	platform_fee : "platformFee?", 
+	stripe_fees : "stripeFees?", 
+	currency : "currency?", 
+	invoice_url : "invoiceUrl?" 
   }
 
 
