dfa62c18aee3b436f7a601967a1daeee866a39f3
debu3223 <debabratamukherjee@trakinvest.com>
2021-04-16 16:17:42 +0530

    modified communityBill mapper

---
+++ communityBillMapper.ts
@@ -0,0 +1,77 @@
+
+const communityBillentityJson = {
+    Id: "Id?",
+    communityId:"communityId?",
+    billDate:"billDate?",
+    title:"title?",
+    description:"description?",
+    totalUsers:"totalUsers?",
+    perUserFee:"perUserFee?",
+    tax:"tax?",
+    totalCharges:"totalCharges?",
+    invoiceUrl:"invoiceUrl?",
+    additionalDetails:"additionalDetails?",
+    ModifiedBy: "ModifiedBy?",
+    CreatedBy: "CreatedBy?",
+    CreationDate: "CreationDate?",
+    ModifiedDate: "ModifiedDate?"
+  };
+  
+  const communityBilltoJson = {
+    Id: "Id?",
+    communityId:"communityId?",
+    billDate:"billDate?",
+    title:"title?",
+    description:"description?",
+    totalUsers:"totalUsers?",
+    perUserFee:"perUserFee?",
+    tax:"tax?",
+    totalCharges:"totalCharges?",
+    invoiceUrl:"invoiceUrl?",
+    additionalDetails:"additionalDetails?",
+    ModifiedBy: "ModifiedBy?",
+    CreatedBy: "CreatedBy?",
+    CreationDate: "CreationDate?",
+    ModifiedDate: "ModifiedDate?"
+  };
+  
+  const communityBillentityToDtoJson = {
+    Id: "Id?",
+    communityId:"communityId?",
+    billDate:"billDate?",
+    title:"title?",
+    description:"description?",
+    totalUsers:"totalUsers?",
+    perUserFee:"perUserFee?",
+    tax:"tax?",
+    totalCharges:"totalCharges?",
+    invoiceUrl:"invoiceUrl?",
+    additionalDetails:"additionalDetails?",
+    ModifiedBy: "ModifiedBy?",
+    CreatedBy: "CreatedBy?",
+    CreationDate: "CreationDate?",
+    ModifiedDate: "ModifiedDate?"
+  };
+  
+  const communityBilltoToEntityJson = {
+    Id: "Id?",
+    communityId:"communityId?",
+    billDate:"billDate?",
+    title:"title?",
+    description:"description?",
+    totalUsers:"totalUsers?",
+    perUserFee:"perUserFee?",
+    tax:"tax?",
+    totalCharges:"totalCharges?",
+    invoiceUrl:"invoiceUrl?",
+    additionalDetails:"additionalDetails?",
+    ModifiedBy: "ModifiedBy?",
+    CreatedBy: "CreatedBy?",
+    CreationDate: "CreationDate?",
+    ModifiedDate: "ModifiedDate?"
+  };
+  
+  module.exports.communityBillentityJson = communityBillentityJson;
+  module.exports.communityBilltoJson = communityBilltoJson;
+  module.exports.communityBillentityToDtoJson = communityBillentityToDtoJson;
+  module.exports.communityBilltoToEntityJson = communityBilltoToEntityJson;
