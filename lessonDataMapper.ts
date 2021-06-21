c9c6cb5164c40bca39a0789647ba5a6fe8d84e6a
AAYUSH SOMANI <aayush@trakinvest.com>
2021-06-21 13:39:13 +0530

    added sequenceIndex

--- lessonDataMapper.ts
+++ lessonDataMapper.ts
@@ -1,85 +1,89 @@
 const lessonDataentityJson = {
-  Id:"Id?",
-  contentType : "contentType?",
-  url : "url?",
-  isReviewed : "isReviewed?",
+  Id: "Id?",
+  contentType: "contentType?",
+  url: "url?",
+  isReviewed: "isReviewed?",
   isSubmitted: "isSubmitted?",
   isDraft: "isDraft?",
   lessonDetails: "lessonDetails?",
-  lessonId:"lessonId?",
-  totalPoints:"totalPoints?",
+  lessonId: "lessonId?",
+  totalPoints: "totalPoints?",
   lessonDataReview: "lessonDataReview?",
   lesson: "lesson?",
-  lessonDataUser:"lessonDataUser?",
+  lessonDataUser: "lessonDataUser?",
   ModifiedBy: "ModifiedBy?",
   CreatedBy: "CreatedBy?",
   CreationDate: "CreationDate?",
-  ModifiedDate: "ModifiedDate?"
+  ModifiedDate: "ModifiedDate?",
+  sequenceIndex: "sequenceIndex?"
   //SocketId: "SocketId?",
-    
+
 };
 
 const lessonDatadtoJson = {
-  Id:"Id?",
-  contentType : "contentType?",
-  url : "url?",
-  isReviewed : "isReviewed?",
+  Id: "Id?",
+  contentType: "contentType?",
+  url: "url?",
+  isReviewed: "isReviewed?",
   isSubmitted: "isSubmitted?",
   isDraft: "isDraft?",
   lessonDetails: "lessonDetails?",
-  lessonId:"lessonId?",
-  totalPoints:"totalPoints?",
+  lessonId: "lessonId?",
+  totalPoints: "totalPoints?",
   lessonDataReview: "lessonDataReview?",
   lesson: "lesson?",
-  lessonDataUser:"lessonDataUser?",
+  lessonDataUser: "lessonDataUser?",
   ModifiedBy: "ModifiedBy?",
   CreatedBy: "CreatedBy?",
   CreationDate: "CreationDate?",
-  ModifiedDate: "ModifiedDate?"
+  ModifiedDate: "ModifiedDate?",
+  sequenceIndex: "sequenceIndex?"
   //SocketId: "SocketId?",
-    
+
 };
 
 const lessonDataentityToDtoJson = {
-  Id:"Id?",
-  contentType : "contentType?",
-  url : "url?",
-  isReviewed : "isReviewed?",
+  Id: "Id?",
+  contentType: "contentType?",
+  url: "url?",
+  isReviewed: "isReviewed?",
   isSubmitted: "isSubmitted?",
   isDraft: "isDraft?",
   lessonDetails: "lessonDetails?",
-  lessonId:"lessonId?",
-  totalPoints:"totalPoints?",
+  lessonId: "lessonId?",
+  totalPoints: "totalPoints?",
   lessonDataReview: "lessonDataReview?",
   lesson: "lesson?",
-  lessonDataUser:"lessonDataUser?",
+  lessonDataUser: "lessonDataUser?",
   ModifiedBy: "ModifiedBy?",
   CreatedBy: "CreatedBy?",
   CreationDate: "CreationDate?",
-  ModifiedDate: "ModifiedDate?"
+  ModifiedDate: "ModifiedDate?",
+  sequenceIndex: "sequenceIndex?"
   //SocketId: "SocketId?",
-    
+
 };
 
 const lessonDatadtoToEntityJson = {
-  Id:"Id?",
-  contentType : "contentType?",
-  url : "url?",
-  isReviewed : "isReviewed?",
+  Id: "Id?",
+  contentType: "contentType?",
+  url: "url?",
+  isReviewed: "isReviewed?",
   isSubmitted: "isSubmitted?",
   isDraft: "isDraft?",
   lessonDetails: "lessonDetails?",
-  lessonId:"lessonId?",
-  totalPoints:"totalPoints?",
+  lessonId: "lessonId?",
+  totalPoints: "totalPoints?",
   lessonDataReview: "lessonDataReview?",
   lesson: "lesson?",
-  lessonDataUser:"lessonDataUser?",
+  lessonDataUser: "lessonDataUser?",
   ModifiedBy: "ModifiedBy?",
   CreatedBy: "CreatedBy?",
   CreationDate: "CreationDate?",
-  ModifiedDate: "ModifiedDate?"
+  ModifiedDate: "ModifiedDate?",
+  sequenceIndex: "sequenceIndex?"
   //SocketId: "SocketId?",
-    
+
 };
 
 module.exports.lessonDataentityJson = lessonDataentityJson;
