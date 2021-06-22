108cd5521980750ddebfc07ecc8ea540b6ae84cf
Subhash <subhash.d@trakinvest.com>
2021-06-22 14:14:27 +0530

    added nuvepro user mapper

---
+++ nuveproUserMapper.ts
@@ -0,0 +1,60 @@
+const nuveprouserentityJson = {
+  Id: "Id?",
+  nuveproId: "nuveproId",
+  userId: "userId",
+  password: "password",
+  loginId: "loginId",
+  teamIds: "teamIds",
+  subscriptions: "subscriptions",
+  ModifiedBy: "ModifiedBy?",
+  CreatedBy: "CreatedBy?",
+  CreationDate: "CreationDate?",
+  ModifiedDate: "ModifiedDate?",
+};
+
+const nuveprouserdtoJson = {
+  Id: "Id?",
+  nuveproId: "nuveproId",
+  userId: "userId",
+  password: "password",
+  loginId: "loginId",
+  teamIds: "teamIds",
+  subscriptions: "subscriptions",
+  ModifiedBy: "ModifiedBy?",
+  CreatedBy: "CreatedBy?",
+  CreationDate: "CreationDate?",
+  ModifiedDate: "ModifiedDate?",
+};
+
+const nuveprouserentityToDtoJson = {
+  Id: "Id?",
+  nuveproId: "nuveproId",
+  userId: "userId",
+  password: "password",
+  loginId: "loginId",
+  teamIds: "teamIds",
+  subscriptions: "subscriptions",
+  ModifiedBy: "ModifiedBy?",
+  CreatedBy: "CreatedBy?",
+  CreationDate: "CreationDate?",
+  ModifiedDate: "ModifiedDate?",
+};
+
+const nuveprouserdtoToEntityJson = {
+  Id: "Id?",
+  nuveproId: "nuveproId",
+  userId: "userId",
+  password: "password",
+  loginId: "loginId",
+  teamIds: "teamIds",
+  subscriptions: "subscriptions",
+  ModifiedBy: "ModifiedBy?",
+  CreatedBy: "CreatedBy?",
+  CreationDate: "CreationDate?",
+  ModifiedDate: "ModifiedDate?",
+};
+
+module.exports.nuveprouserentityJson = nuveprouserentityJson;
+module.exports.nuveprouserdtoJson = nuveprouserdtoJson;
+module.exports.nuveprouserentityToDtoJson = nuveprouserentityToDtoJson;
+module.exports.nuveprouserdtoToEntityJson = nuveprouserdtoToEntityJson;
